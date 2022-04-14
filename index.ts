c174198a4c17be35def0258248b76b728ede4943
inqbs <incubass64@gmail.com>
2022-04-14 15:10:09 +0900

    feat: using opentype's function to draw

--- index.ts
+++ index.ts
@@ -2,8 +2,7 @@ import 'dotenv/config'
 
 import * as fs from 'fs';
 import * as glob from 'glob'
-import * as opentype from 'opentype.js'
-import { Font } from 'opentype.js'
+import { load, Font } from 'opentype.js'
 import { registerFont, createCanvas } from 'canvas'
 
 const lang = process.env.LANGUAGE
@@ -15,44 +14,49 @@ const canvasSize = {
 }
 const canvas = createCanvas(+canvasSize.width, +canvasSize.height)
 
+/**
+ * Draw and export image of fontname as font.
+ * @param font: Font
+ * @returns result: Promise<boolean>
+ */
 const makeOutput = (font: Font) => {
   const ctx = canvas.getContext('2d')
 
-  const {fullName: fullNames, fontFamily: fontFamilies, postScriptName: postScriptNames} = font.names
+  const {fullName: fullNames, postScriptName: postScriptNames} = font.names
 
   const fullName = getLangName(fullNames)
-  const fontFamily = getLangName(fontFamilies)
-  const postScriptName = getLangName(postScriptNames)
+  const fontSize = +(process.env.FONT_SIZE) || 24
 
-  const fontSize = process.env.FONT_SIZE || 24
-
-  ctx.font = `${fontSize}px "${fontFamily}"`
   const text = fullName
   const textPosition = {
-    x: canvas.width / 2,
-    y: canvas.height / 2
+    x: 0,
+    y: canvas.height - fontSize / 2
   }
-  ctx.textAlign = "center";
-  ctx.textBaseline = "middle";
-  ctx.fillText(text, textPosition.x, textPosition.y)
+
+  font.draw(ctx, text, textPosition.x, textPosition.y, fontSize)
+
+  const postScriptName = getLangName(postScriptNames)
 
   return new Promise(resolve => {
     const out = fs.createWriteStream(`${__dirname}/output/${postScriptName}.png`)
     const stream = canvas.createPNGStream()
     stream.pipe(out)
     out.on('finish', () => {
-      console.log(`[output-over] ${postScriptName}`)
+      console.debug(`[output-over] ${postScriptName}`)
       ctx.clearRect(0, 0, canvas.width, canvas.height)
       resolve(true)
     })
   })
 }
 
+/**
+ * Load input's fonts and export image their font's images.
+ */
 export const exportImages = async () => {
   const files = glob.sync('input/*.@(TTF|ttf|OTF|otf)')
 
   for (const file of files) {
-    const font = await opentype.load(file)
+    const font = await load(file)
 
     registerFont(file, { family: getLangName(font.names.fontFamily) })
     await makeOutput(font)
